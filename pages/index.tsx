ed1307ff4f6438d2ad1a16bed0237d76809531ec
Manuel Bichler <mbichler@cemm.at>
2021-01-13 12:40:46 +0100

    delete props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,8 @@ import Head from "next/head";
 import Container from "../components/container";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
-import Post from "../types/post";
 
-type Props = {
-  allPosts: Post[];
-};
-
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+const Index = () => {
   return (
     <>
       <Layout>
